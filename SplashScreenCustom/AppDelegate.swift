85cc6782bb15c696b41df545d85a4d62bf5627c9
Nikhil boriwale <boriwale.n@gmail.com>
2018-07-11 11:09:05 +0530

    customesplashscreenwithtime
    customesplashscreenwithtime Swift

--- SplashScreenCustom/AppDelegate.swift
+++ SplashScreenCustom/AppDelegate.swift
@@ -15,10 +15,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        self.splashscreen()
+        
         return true
     }
-
+// SPLASH SCREEN ka code yha pay likhna hey
+       //  1. identifire set kro splashscreen ko
+       // 2. identifire set kro main.storyboard key view controller  ko
+    private func splashscreen(){
+        
+        let launchScreenVC = UIStoryboard.init(name: "LaunchScreen",bundle:nil)
+        let rootVC = launchScreenVC.instantiateViewController(withIdentifier: "splashController")
+        self.window?.rootViewController = rootVC
+        self.window?.makeKeyAndVisible()
+        Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(dismissSplashController), userInfo: nil, repeats: false)
+    }
+    @objc func dismissSplashController(){
+        let mainVC = UIStoryboard.init(name:"Main",bundle:nil)
+        let rootVC  = mainVC.instantiateViewController(withIdentifier: "initController")
+        self.window?.rootViewController = rootVC
+        self.window?.makeKeyAndVisible()
+    }
+    
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
